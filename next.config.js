d3abd66f7a83e0ca5fdac3d7425802c503dd00df
Mohammed Abdul Rafe Sajid <abdulrafesajid@gmail.com>
2026-07-02 14:34:53 +0530

    fix next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,3 @@
-/** @type {import('next').NextConfig} */
 const withPWA = require('next-pwa')({
   dest: 'public',
   disable: process.env.NODE_ENV === 'development',
@@ -6,21 +5,47 @@ const withPWA = require('next-pwa')({
   skipWaiting: true,
 });
 
-const nextConfig = {
-  output: 'standalone',
+module.exports = withPWA({
   reactStrictMode: true,
   eslint: {
     ignoreDuringBuilds: true,
   },
   typescript: {
     ignoreBuildErrors: true,
-    tsconfigPath: './tsconfig.json',
   },
-  serverExternalPackages: ['@prisma/client', 'prisma'],
-  outputFileTracingIncludes: {
-    '/api/**/*': ['./node_modules/.prisma/client/**/*'],
-    '/*': ['./node_modules/.prisma/client/**/*'],
-  },
-};
+});
 
-module.exports = withPWA(nextConfig);
+// /** @type {import('next').NextConfig} */
+// const withPWA = require('next-pwa')({
+//   dest: 'public',
+//   disable: process.env.NODE_ENV === 'development',
+//   register: true,
+//   skipWaiting: true,
+// });
+
+// // const nextConfig = {
+// //   output: 'standalone',
+// //   reactStrictMode: true,
+// //   eslint: {
+// //     ignoreDuringBuilds: true,
+// //   },
+// //   typescript: {
+// //     ignoreBuildErrors: true,
+// //     tsconfigPath: './tsconfig.json',
+// //   },
+// //   serverExternalPackages: ['@prisma/client', 'prisma'],
+// //   outputFileTracingIncludes: {
+// //     '/api/**/*': ['./node_modules/.prisma/client/**/*'],
+// //     '/*': ['./node_modules/.prisma/client/**/*'],
+// //   }
+// const nextConfig = {
+//   reactStrictMode: true,
+//   eslint: {
+//     ignoreDuringBuilds: true,
+//   },
+//   typescript: {
+//     ignoreBuildErrors: true,
+//   },
+// };
+
+// module.exports = withPWA(nextConfig);
